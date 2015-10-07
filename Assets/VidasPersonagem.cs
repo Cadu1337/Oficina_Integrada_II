22316be958f7094af3fa84ba7b95ef8105d4e4a9
Isaquinn <isaquearaujo22@gmail.com>
2015-10-06 22:01:15 -0300

    Mudança de Cenário
    Alterei O cenário

--- Assets/VidasPersonagem.cs
+++ Assets/VidasPersonagem.cs
@@ -17,11 +17,23 @@ public class VidasPersonagem : MonoBehaviour {
 		//spritesenemy.sprite = Resources.Load<Sprite>("SpriteVidasAranha");
 		if(monstroativo == "Aranha0")
 		{
-			if(Espada.vidasaranha[0] == 2)
+			if(Espada.vidasaranha[0] == 3)
 			{
-				spritesenemy.sprite = Resources.Load<Sprite>("SpriteVidasAranha_1");
+                spritesenemy.sprite = Resources.Load<Sprite>("Cópia de Bara de vida_001");
 
 			}
+            else if(Espada.vidasaranha[0] == 2)
+            {
+                spritesenemy.sprite = Resources.Load<Sprite>("Cópia de Bara de vida_002");
+            }
+            else if(Espada.vidasaranha[0] == 1)
+            {
+                spritesenemy.sprite = Resources.Load<Sprite>("Cópia de Bara de vida_003");
+            }
+            else if(Espada.vidasaranha[0] == 0)
+            {
+                spritesenemy.sprite = Resources.Load<Sprite>("Cópia de Bara de vida_004");
+            }
 		}
 
 	
